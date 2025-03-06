a19002d5a4c19a7ff7c6c51989b18048740ce6fa
uptonking <jinyaoo86@gmail.com>
2025-03-06 19:33:45 +0800

    chore: react input events order - keydown > oninput > cbRef > keyup

--- nodejs-examples/file-read.mjs
+++ nodejs-examples/file-read.mjs
@@ -1,9 +1,18 @@
 import { readdir } from 'node:fs/promises';
+import { statSync } from 'node:fs';
 
 try {
   // const files = await readdir('.'); // not work
   const files = await readdir('../');
-  console.log(';; import.meta.dirname ',import.meta.dirname)
+  console.log(';; import.meta.dirname ', import.meta.dirname);
+  // console.log(
+  //   ';; statSync-filename ',
+  //   statSync(import.meta.filename).isDirectory(),
+  // );
+  // console.log(
+  //   ';; statSync-dirname ',
+  //   statSync(import.meta.dirname).isDirectory(),
+  // );
   for (const file of files) console.log(file);
 } catch (err) {
   console.error(err);
